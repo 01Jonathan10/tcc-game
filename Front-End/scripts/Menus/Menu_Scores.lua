b7ac5ad0b167696f3289da230cad8777d64ecda2
01Jonathan10 <jonathansuper1@gmail.com>
2021-01-29 18:44:35 -0300

    Scores Menu

--- Front-End/scripts/Menus/Menu_Scores.lua
+++ Front-End/scripts/Menus/Menu_Scores.lua
@@ -64,7 +64,7 @@ end
 function ScoresMenu:confirm_claim_btn()
 	return {
 		x = 980,y = 580, w = 180, h = 50, text={{0,0,0,1}, ("Claim"):translate()},
-		click = function() self:claim_score(score) end, text_size = 1.7
+		click = function() self:claim_score() end, text_size = 1.7
 	}
 end
 
@@ -213,8 +213,8 @@ function ScoresMenu:draw_score(score,x,y)
 	View.setColor(1,1,1)
 end
 
-function ScoresMenu:claim_score(score)
-	API.claim_score(score):after(function(response)
+function ScoresMenu:claim_score()
+	API.claim_score(self.selected_score):after(function()
 		API.load_scores():success(function(response)
 			self.semesters = response
 			self:reload_buttons()
