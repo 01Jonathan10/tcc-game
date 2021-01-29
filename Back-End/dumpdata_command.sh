b49ec2ea50342382561b545d3b2442e1ea7709f5
01Jonathan10 <01jonathanh10@gmail.com>
2021-01-29 11:12:03 -0300

    Dump data command update

--- Back-End/dumpdata_command.sh
+++ Back-End/dumpdata_command.sh
@@ -1,1 +1,1 @@
-python manage.py dumpdata --format json --exclude API.player --exclude API.iteminstance --exclude API.questinstance --exclude API.task --exclude API.taskinstance --exclude API.taskreview --exclude API.score API > dump_data.json
+python manage.py dumpdata --format json --exclude API.iteminstance --exclude API.questinstance --exclude API.task --exclude API.taskinstance --exclude API.taskreview --exclude API.score API > dump_data.json
