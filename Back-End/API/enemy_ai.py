abd2397ac11df6c08a369ab962831f7ddd1b9591
01Jonathan10 <jonathansuper1@gmail.com>
2021-03-04 16:58:58 -0300

    Better enemy AI

--- Back-End/API/enemy_ai.py
+++ Back-End/API/enemy_ai.py
@@ -1,7 +1,13 @@
+from API.constants import ObjType
+
+
 def get_ai_actions(character, board_state, char_id):
     actions = [{}, {}, {'action': 'pass'}]
     actions[0] = get_move_action(character, board_state, char_id)
     actions[1] = get_attack_action(character, board_state, char_id)
+
+    actions = [action for action in actions if action]
+
     return actions
 
 
@@ -16,6 +22,12 @@ def get_move_action(character, board_state, char_id):
     else:
         new_position['y'] += ai_var - 2
 
+    for object in board_state['objects']:
+        if object['position']['x'] == new_position['x'] and object['position']['y'] == new_position['y']:
+            return None
+
+    character['new_position'] = new_position
+
     return {
         "action": "move",
         "position": character['position'],
@@ -24,9 +36,29 @@ def get_move_action(character, board_state, char_id):
 
 
 def get_attack_action(character, board_state, char_id):
-    return {
+    position = character.get('new_position') or character['position']
+    if character.get('new_position'):
+        del character['new_position']
+
+    action = {
         "action": "attack",
-        "position": character['position'],
-        "direction": 3,
+        "position": position,
         "skill": character['skills'][0]
     }
+
+    directions = [
+        [position['x'] + 1, position['y']],
+        [position['x'], position['y'] + 1],
+        [position['x'] - 1, position['y']],
+        [position['x'], position['y'] - 1]
+    ]
+
+    for object in board_state['objects']:
+        for direction in range(4):
+            if object['position']['x'] == directions[direction][0] and object['position']['y'] == directions[direction][1]:
+                if object['type'] == ObjType.PLAYER:
+                    action['direction'] = direction + 1
+                    print(action)
+                    return action
+
+    return None
